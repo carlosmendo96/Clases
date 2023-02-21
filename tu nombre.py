bc3ca5f6cf15ebcad1124d69f4b10fa328073766
carlosmendo96 <124932770+carlosmendo96@users.noreply.github.com>
2023-02-21 12:16:48 -0600

    Add files via upload

---
+++ tu nombre.py
@@ -0,0 +1,4 @@
+#Programa que da tu nombre
+a = input("Give me your first name \n")
+b = input("Give me your last name \n")
+print("Hello " + str(a ) + " " + str(b) + "!" + " " + "You just delved into python.")
